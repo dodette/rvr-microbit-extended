436e2a92764c3aa890ae6ce8145b5bd9a02967a0
Odette Maher <do.dot.dodette@gmail.com>
2022-07-12 16:14:22 +1000

    First iteration of drive-to-position command

---
+++ constants_ext.ts
@@ -0,0 +1,5 @@
+namespace sphero {
+    export class DriveCommandsExt {
+        public static readonly driveToPositionSI: number = 0x38;
+    }
+}
---
+++ rvr_ext.ts
@@ -0,0 +1,31 @@
+//% weight=100 color=#C28DF0 icon="\uf120" block="RVR Extensions"
+namespace sphero {
+    //% block="Drive to position ( %x|m, %y|m ) at speed %speed| and face heading %heading|"
+    //% speed.min=0 speed.max=2
+    //% heading.min=0 heading.max=359
+    //% subcategory=Movement
+    export function driveToPosition(heading: number, x: number, y: number, speed: number): void {
+        // For other drive functions, the flag 0x01 is used to indicate reverse. It is not needed
+        // to drive to an internal reference position
+        let flags: number = 0x00;
+
+        let messageData: Array<number> = [speed];
+        let headingArray: Array<number> = Utilities.int16ToByteArray(heading);
+
+        for (let i: number = 0; i < headingArray.length; i++) {
+            messageData.push(headingArray[i]);
+        }
+
+        messageData.push(flags);
+
+        let apiMessage = buildApiCommandMessageWithDefaultFlags(
+            ApiTargetsAndSources.robotStTarget,
+            ApiTargetsAndSources.serviceSource,
+            DriveCommands.driveDeviceId,
+            DriveCommandsExt.driveToPositionSI,
+            messageData
+        );
+
+        serial.writeBuffer(pins.createBufferFromArray(apiMessage.messageRawBytes));
+    }
+}
